e4c7d911a2efac3cf2a07c861c1c46208578bcbf
1972859718 <33173163+1972859718@users.noreply.github.com>
2018-04-06 14:40:54 +0800

    循环结构以及其高级用法

---
+++ week_4/for_in_else.py
@@ -0,0 +1,15 @@
+for c in "python":
+    if c == 't':
+        continue
+    print(c, end = ' ')
+else:
+    print("well done")
+
+###########################
+
+for c in "python":
+    if c == 't':
+        break
+    print(c, end = ' ')
+else:
+    print("well done")

efc73f4bfce5e852bc0026638953d929fc9a1309
1972859718 <33173163+1972859718@users.noreply.github.com>
2018-04-12 17:14:34 +0800

    科赫雪花

---
+++ week_5/KochDrawV2.py
@@ -0,0 +1,21 @@
+#KochDrawV2.py
+import turtle as t
+def koch(size, n):
+    if n == 0:
+        t.fd(size)
+    else:
+        for angle in [0, 60, -120, 60]:
+            t.left(angle)
+            koch(size/3, n-1)
+def main():
+    t.setup(600,600)
+    t.penup()
+    t.goto(-200,100)
+    t.pendown()
+    t.pensize(2)
+    level = 3       #3阶科赫曲线
+    for i in range(3):
+        koch(400,level)
+        t.right(120)
+    t.hideturtle()
+main()

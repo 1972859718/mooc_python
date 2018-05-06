d214a8d7fabbdcc977380aca4a71ab3d7e15dbaa
1972859718 <33173163+1972859718@users.noreply.github.com>
2018-05-06 22:26:39 +0800

    个性化样式词云

---
+++ week_7/GovRptWordCloudv2.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  6 22:04:35 2018
+
+@author: moon
+"""
+
+#GovRptWordCloudv2.py
+import jieba
+import wordcloud
+from scipy.misc import imread
+mask = imread("fivestar.jpg")
+f = open("新时代中国特色社会主义.txt", "r", encoding="utf-8")
+t = f.read()
+f.close()
+ls = jieba.lcut(t)
+txt = " ".join(ls)
+w = wordcloud.WordCloud(font_path = "msyh.ttc", mask = mask, width = 1000, height = 700, background_color = "white")
+w.generate(txt)
+w.to_file("grwordcloud_new1.png")
